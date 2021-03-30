5d43b0421f0b1dd9b7289b313b647eeffb732f1d
Farshad Zargari <Farshadzargary1997@gmail.com>
2021-03-30 21:40:14 +0430

    UI added

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
-import tkinter
+import pyperclip
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import threading
@@ -14,19 +15,32 @@ if __name__ == "__main__":
     win.resizable(False, False)
     
     #title
-    
+    Label_title = Label(win, text = "Youtube Downloader", fg = "red", font = ("Times",30)).place(x = 90, y = 25)
     
     #URL
-    label_link = Label(win, text = "Link").place(x = 45, y = 50)
+    URL_label = Label(win, text = "URL:").place(x = 70, y = 100)
     URL_VAR = StringVar()
-    URL_entry = Entry(win, textvariable = URL_VAR)
-    URL_entry.place(x = 115, y = 50)
+    URL_entry = Entry(win, textvariable = URL_VAR, width = 50).place(x = 115, y = 100)
+    URL_button = ttk.Button(win, text = "Get URL").place(x = 220, y = 125)
+    #Paste Button
+    paste_ico = PhotoImage(file = r"clipboard_paste.png")
+    Paste_button = ttk.Button(win, image = paste_ico, width = 3).place(x = 420, y = 98)
 
-    label2 = Label(win, text = "delay time:").place(x = 45, y = 90)
-    delayvar = IntVar()
-    delayent = Entry(win, textvariable = delayvar)
-    delayent.place(x = 115, y = 90)
+    #save to
+    Path_label = Label(win, text = "Save to:").place(x = 60, y = 160)
+    Path_Var = StringVar()
+    Path_Ent = ttk.Entry(win, textvariable = Path_Var, width = 50, state = DISABLED).place(x = 115, y = 160)
+    command_button = ttk.Button(win, text = "Save to").place(x = 220, y = 185)
+    
+    #Option menu
+    qualities_Var = StringVar()
+    qualities_Var.set("-")
+    qualities_label = Label(win, text = "Qualities:").place(x = 60, y = 210)
+    options = ttk.OptionMenu(win, variable = qualities_Var).place(x = 115, y = 210)
+    
+    #Progressbar
+    pb_Var = DoubleVar()
+    pb = ttk.Progressbar(win, variable = pb_Var, length = 300).place(x = 115, y = 450)
 
-    command_button = ttk.Button(win, text = "Run").place(x = 100, y = 110)
 
     win.mainloop()
